c6d1f865b8966b7b36da3c2b7b168eb30cbca1d6
Cristian Salazar <cristiancamilosalazararenas@outlook.com>
2026-02-15 21:13:21 -0500

    Se crearon nuevos métodos en la clase NameProcessor

--- TCP-Server/src/main/java/com/cristian/business/NameProcessor.java
+++ TCP-Server/src/main/java/com/cristian/business/NameProcessor.java
@@ -7,8 +7,32 @@ public class NameProcessor implements IMessageProcessor{
             return "[ERROR] Error procesando el mensaje";
         }
         String[] parts = message.split("#");
-        String name = parts[0];
-        String lastName = parts[1];
-        return "Hola %s %s".formatted(name, lastName);
+        String requestType = parts[0];
+        IInfoExporter exporterType = this.createExporterForType(requestType);
+        exporterType.export(parts);
+        return personalizedResponse(exporterType);
+    }
+
+    private IInfoExporter createExporterForType(String requestType){
+        switch (requestType){
+            case "REGISTRAR_PACIENTE":
+                return new CSVPacienteExporter();
+            case "REGISTRAR_VIRUS":
+                return new FastaRegistroVirusExport();
+            case "REGISTRAR_MUESTRA":
+                return new FastaDiagnosticoGenetico();
+            default:
+                throw new IllegalArgumentException("La solicitud '" + requestType + "' es desconocida.");
+        }
+    }
+
+    private String personalizedResponse(IInfoExporter exporterType){
+        if(exporterType instanceof CSVPacienteExporter){
+            return "Paciente registrado correctamente...";
+        } else if (exporterType instanceof FastaRegistroVirusExport){
+            return "Virus registrado correctamente...";
+        } else {
+            return "Muestra registrada correctamente...";
+        }
     }
 }
